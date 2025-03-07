00faa3f7b7ebbddeff16f55d9791638477132971
Daviion9 <daviionburks9@gmail.com>
2025-03-06 23:43:19 -0500

    newb19

---
+++ 19.js
@@ -0,0 +1,40 @@
+const freelancers = [
+    { name: "Dr. Slice", price: 50, occupation: "gardener" },
+    { name: "Dr. Pressure", price: 120, occupation: "programmer" },
+    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+    { name: "Prof. Prism", price: 43, occupation: "teacher" },
+    { name: "Dr. Impulse", price: 53, occupation: "teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "programmer" },
+    { name: "Dr. Wire", price: 91, occupation: "teacher" },
+    { name: "Goose", price: 12, occupation: "driver" },
+  ];
+  console.log(freelancers);
+
+  /*I want to create an array for each occupation */
+  Const = Teacher {
+    name: (Prof.Prism, Dr.Impulse, Dr.Wire),
+    Price: ($43,$53,$91)
+    
+    console.log(Teacher)
+  };
+  
+  Const = Programmer  {
+    name:(Prof.Spark, Dr.Pressure)
+    Price:($76,$120)
+
+    console.log(Programmer)
+  };
+
+  Const = Gardener {
+    name:(Dr.Slice)
+    Price:($50)
+
+    console.log(Gardener)
+  };
+
+  Const = Driver {
+    name:Goose
+    price:($12)
+  };
+  
+  /* Then I want to calculate the average price within each of the arrays  */
